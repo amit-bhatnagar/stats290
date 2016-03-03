0dbaa34b02e32663b8aa16b5cc0dc121942c0352
K B <kavithab@microsoft.com>
2016-03-02 22:49:41 -0800

    Getting paged results

--- R/RestaurantBase.R
+++ R/RestaurantBase.R
@@ -7,7 +7,11 @@ factualAPIKey = "mKxC6I9lTWnKNTSNF12e3keaWblCXqoaZ1qROdVo"
 #works baseURL <- "http://api.v3.factual.com/t/restaurants-us?q=Indian,seattle"
 #Works baseURL <- "http://api.v3.factual.com/t/restaurants-us?filters={\"name\":{\"$bw\":\"star\"}}&include_count=true"
 #Does not work baseURL <- "http://api.v3.factual.com/t/restaurants-us?q=Coffee,\"Los Angeles\""
+#Do a full-text search of the restaurant database for rows that match the word coffee and the phrase los angeles:
 baseURL <- "http://api.v3.factual.com/t/restaurants-us?q=Coffee,\"California\""
+#To support paging in your app, return rows 20-40 of the full-text search result above:
+#Works baseURL <- "http://api.v3.factual.com/t/restaurants-us?q=Coffee,\"Seattle\"&offset=20&limit=20"
+
 
 #http://api.v3.factual.com/t/restaurants-us?q=Coffee,"Los Angeles"
 #query = "Coffee, Seattle"
