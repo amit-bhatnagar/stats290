ca00f7c8e6f17d6850022ce725a424a9e44401ca
Amit Bhatnagar <ab@amitbhatnagar.com>
2016-03-06 12:17:48 -0800

    URL Encoding makes multi-string query work!

--- R/first.R
+++ R/first.R
@@ -3,16 +3,16 @@
 
 getQuerybyRegion<-function(query){
 library(jsonlite)
+query = URLencode(query)
 
 factualAPIKey = "mKxC6I9lTWnKNTSNF12e3keaWblCXqoaZ1qROdVo"
-
 
 baseURL <- "http://api.v3.factual.com/t/places/facets?select=region"
 URL=paste0(baseURL,"&q=",query,"&KEY=",factualAPIKey)
 
 getData=fromJSON(URL, flatten = TRUE)
 
-dataByRegion = table(getData$response$data$region)
+dataByRegion = as.data.frame(getData$response$data$region)
 
  print(dataByRegion)
 }
@@ -20,16 +20,16 @@ dataByRegion = table(getData$response$data$region)
 
 getQuerybyLocality<-function(query){
   library(jsonlite)
+  query = URLencode(query)
   
   factualAPIKey = "mKxC6I9lTWnKNTSNF12e3keaWblCXqoaZ1qROdVo"
   
   baseURL <- "http://api.v3.factual.com/t/places/facets?select=locality"
   
-  
-  
   URL=paste0(baseURL,"&q=",query,"&KEY=",factualAPIKey)
   
   getData=fromJSON(URL, flatten = TRUE)
-  dataByLocality = table(getData$response$data$locality)
+  dataByLocality = as.data.frame(getData$response$data$locality)
+  
   print(dataByLocality)
 }
