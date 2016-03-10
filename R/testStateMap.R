70b7c4c76fb8cee3095d6cb842ef84fa5efa9d6c
Amit Bhatnagar <ab@amitbhatnagar.com>
2016-03-09 22:06:54 -0800

    Fixing code of yesterday's mapping functions
    Somehow an older versions of files got checked in, and even my local
    files had reverted to old versions. Checking in the correct files now

--- R/testStateMap.R
+++ R/testStateMap.R
@@ -1,28 +1,31 @@
-library(XML)
-library(ggplot2)
-library(plyr)
-library(maps)
 
-unemp <-
-  readHTMLTable('http://www.bls.gov/web/laus/laumstrk.htm',
-                colClasses = c('character', 'character', 'numeric'))[[2]]
+testComparisonByState<-function(){
 
-names(unemp) <- c('rank', 'region', 'rate')
-unemp$region <- tolower(unemp$region)
+library(ggmap)
 
+query1 = "Subway"
+query2 = "Chipotle"
+
+testRand=runif(50)
+Winner = ifelse(testRand<0.3, query1,query2)
+names(Winner) = state.name
+color <- ifelse(Winner==query1,"red","green")
+mydf = as.data.frame(Winner)
+mydf$color=color
+
+
+states <- data.frame(region = tolower(state.name),latitude=state.center$y,longitude=state.center$x)
+rownames(states) <-state.abb
+chainDataWithLatLong = cbind(states,mydf)
 us_state_map <- map_data('state')
-map_data <- merge(unemp, us_state_map, by = 'region')
 
-map_data <- arrange(map_data, order)
 
-states <- data.frame(state.center, state.abb)
-
-p1 <- ggplot(data = map_data, aes(x = long, y = lat, group = group))
-p1 <- ggplot(data = map_data, aes(x = long, y = lat))
-p1 <- p1 + geom_polygon(aes(fill ="red"))
-p1 <- p1 + geom_path(colour = 'gray', linemitre  = 2)
-# p1 <- p1 + scale_fill_brewer('Unemployment Rate (Jan 2011)', palette  = 'PuRd')
+finalData <- merge(chainDataWithLatLong, us_state_map, by = 'region')
+p1 <- ggplot(data = finalData, aes(x = long, y = lat, group = group))
+p1 <- p1 + geom_polygon(aes(fill = color), show.legend = FALSE)
+p1 <- p1 + geom_path(colour = 'goldenrod1', linemitre  = 2)
 p1 <- p1 + coord_map()
-p1 <- p1 + geom_text(data = states, aes(x = x, y = y, label = state.abb, group = NULL), size = 2)
+p1 <- p1 + geom_text(data = chainDataWithLatLong, aes(x = longitude, y = latitude, label = Winner, group=NULL, fontface = "bold"))
 p1 <- p1 + theme_bw()
 p1
+}
