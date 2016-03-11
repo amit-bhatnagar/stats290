ac009fe163f4eb11e1a1f0de03b97b38e7527778
Amit Bhatnagar <ab@amitbhatnagar.com>
2016-03-10 17:00:44 -0800

    Almost working code for getting restaurants near a location
    Currently URL is correctly getting generated and if I paste the URL in
    browser, i get expected result. But somehow this is not working in R.

---
+++ R/getRestaurantsNearLocation.R
@@ -0,0 +1,31 @@
+getRestaurantsNearLocation<-function(pointName, category){
+  
+  library(jsonlite)
+  library(ggmap)
+  pointLatLong = geocode(pointName)
+  pointLatLong = paste(pointLatLong[2],pointLatLong[1],sep = ",")
+  
+  factualAPIKey = "mKxC6I9lTWnKNTSNF12e3keaWblCXqoaZ1qROdVo"
+
+
+  locURL <- paste0("http://api.v3.factual.com/t/places?geo={\"$circle\":{\"$center\":[",pointLatLong,"],\"$meters\": 5000}}")
+
+  category = gsub(" ","+",category)
+  categoryFilter = paste0("{\"category_labels\":{\"$includes\":\"",category,"\"}}")
+  
+  filters=paste0("{\"$and\":[",categoryFilter,"]}")
+  
+
+  limit=20
+  offset=0
+  URL=paste0(locURL,"&filters=",filters,"&limit=",limit,"&offset=",offset,"&KEY=",factualAPIKey)
+
+# #   getData=fromJSON(URL, flatten = TRUE)
+#   df1 = as.data.frame(getData$response)
+#    
+#   df2 = cbind(name=df1$data.name,longitude=df1$data.longitude,latitude=df1$data.latitude)
+#   df2 = as.data.frame(df2)
+   
+}
+
+
