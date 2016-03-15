0146b61a3eefb5939ba8bc569c78dde3671705b7
Amit Bhatnagar <ab@amitbhatnagar.com>
2016-03-15 11:37:50 -0700

    Fixes in plotBusinesses
    Now it does not need the city argument. Plots a map centered on the
    selected businesses

--- restaurantR/R/plotBusinesses.R
+++ restaurantR/R/plotBusinesses.R
@@ -1,38 +1,37 @@
 #' Plot business on a map from a city name and a date-frame of business name, longitude and latitude
-#' @param city A city name
 #' @param businessDF a date-frame of business name, longitude and latitude
 #' @import ggplot2 ggmap
 #' @export
 #' @examples
-#' plotBusinesses("Mountain View",MountainView_Indian_Alcohol_Friendly)
+#' plotBusinesses(MountainView_Indian_Alcohol_Friendly)
 
-plotBusinesses <-function(city, businessDF){
-  # library(ggmap)
+plotBusinesses <-function(businessDF){
   out <- tryCatch(
     {
       longitude = latitude = NULL
 
-      citymap = get_map(location = city , zoom = 15, maptype = "roadmap" , source = "google")
+      bbox <- ggmap::make_bbox(businessDF$longitude, businessDF$latitude, f = 0.1)
+
+      citymap = get_map(location = bbox , maptype = "roadmap" , source = "google")
 
       citymap = ggmap(citymap)
 
       citymap = citymap + ggplot2::geom_point(data=businessDF, ggplot2::aes(x=longitude, y=latitude),
-                                              color = 'blue',
-                                              size = 8, alpha = .6)
+                                              color = 'red',
+                                              size = 4, alpha = .6)
       #citymap = citymap  +    ggplot2::geom_text(ggplot2::aes(label=name), data=businessDF, hjust=-1,
       # fontface = 'bold',check_overlap = TRUE)
+
+#https://rstudio-pubs-static.s3.amazonaws.com/10840_89eda656b4494a609dbba609cda0830e.html
       citymap
     },
     error=function(cond) {
       message(cond)
+
       # Choose a return value in case of error
       return(NA)
-    },
-    warning=function(cond) {
-      message(cond)
-      # Choose a return value in case of warning
-      return(NULL)
     }
+
   )#tryCatch
   return(out)
 }
