a55af9d8bc453d192a00de5f245880be05a600c8
Amit Bhatnagar <ab@amitbhatnagar.com>
2016-03-15 19:42:36 -0700

    Updated datasets

---
+++ restaurantR/R/NYC_business_casual.R
@@ -0,0 +1,9 @@
+#' NYC_business_casual data
+#'
+#' Test data containing  restaurants with attire type "business casual" in New York
+#' @name NYC_business_casual
+#' @docType data
+#' @keywords data
+#'
+NULL
+
